dc5b093f958ff001ccb05c3f8446eb6b4f4f0769
Andrew Lock <andrewlock.net@gmail.com>
2020-04-30 21:46:32 +0100

    Add example

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldGenerated.HelloWorld.SayHello();
+        }
+    }
+}
